0707ccdc3eeac13d1fd627c9c74200506ee51d97
atharavpatil77 <2023.atharavp@isu.ac.in>
2023-11-02 14:40:49 +0530

    First version

---
+++ Desktop/Git_Session/c++/sample.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int num1 , num2;
+
+    cout<<"Enter num1 and num2";
+    cin>>num1>>num2;
+
+    cout<<"Addition is  : "<<num1+num2;
+    
+
+    return 0;
+}
